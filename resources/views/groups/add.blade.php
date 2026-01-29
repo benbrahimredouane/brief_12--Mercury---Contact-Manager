3eeb9584bb6f266f892f5836658243ef23b02230
redouane <benbrahimredouane664@gmail.com>
2026-01-29 22:29:56 +0100

    feat: add future maintance if want to add contact to group page

---
+++ resources/views/groups/add.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>add to group</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+    
+</head>
+<body>
+
+<div class="container d-flex justify-content align-items-center w-100">
+<p>not required but i will do it in future</p>
+</div>
+
+<a href="{{ route('groups.index') }}" class="btn btn-secondary">Back to contact</a> 
+
+    
+</body>
+</html>
