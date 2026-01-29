c5abd844186f059840ef90fb1393e9a044adcb01
redouane <benbrahimredouane664@gmail.com>
2026-01-29 22:33:19 +0100

    chore: add migration add colomn group_id to model contact

--- database/migrations/2026_01_27_173030_create_contacts_table.php
+++ database/migrations/2026_01_27_173030_create_contacts_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();                    // auto-increment primary key
             $table->string('name');          // varchar(255), not null
             $table->string('email');         // longer text, can be null
-            $table->string('phone_num');
-            $table->boolean('is_active')->default(true);
+            $table->string('phone');
+            $table->foreignId('group_id')->nullable()->onDelete('set null'); //foreing key refer to group id can be null and it become null if the group deleted
+            $table->boolean('is_active')->default(true); 
             $table->timestamps();            // created_at + updated_at
         });
     }
