1f1be5250a23df7529a6337a47d55cf0520a9f39
redouane <benbrahimredouane664@gmail.com>
2026-01-29 11:24:51 +0100

    feat: add view page for single group

---
+++ resources/views/groups/view.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>single group</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+ 
+
+
+<h1>Group name: {{ $group->name }}</h1>
+
+
+
+
+
+</table>
+ <a href="{{ route('groups.index') }}" class="btn btn-secondary">Back to contact</a>   
+  <a href="" class="btn btn-primary">Add Contact</a>  
+</body>
+</html>
